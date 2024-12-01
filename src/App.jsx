249c808a3e7bf8308c81ac0ae50d82675faaf09e
Ganesh Patil <patil7227ganesh@gmail.com>
2024-12-01 14:31:24 +0530

    user data fetch setup added

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
 import React from 'react'
 import Login from './pages/Login'
-import Navbar from './components/Navbar'
-import HeroSection from './pages/student/HeroSection'
-import { createBrowserRouter } from 'react-router-dom'
-import MainLayout from './Layout/MainLayout'
-import { RouterProvider } from 'react-router'
-import Courses from './pages/student/Courses'
-import MyLearning from './pages/student/MyLearning'
-import ProfilePage from './pages/student/ProfilePage'
 
-const route = createBrowserRouter([
-  {
-    path:"/",
-    element:<MainLayout/>,
-    children : [
-      {
-        path : '/',
-        element:(
-        <>
-          <HeroSection/>
-          <Courses/>
-        </>
-        )
-      },
-      {
-        path:'my-learning',
-        element:<MyLearning/>
-      },
-      {
-        path:"profile",
-        element:<ProfilePage/>
-      }
-    ]
-  },
-  {
-    path : "login",
-    element:<Login/>
-  }
-])
 const App = () => {
   return (
-      <main>
-        <RouterProvider router ={route}/>
-      </main>
+      <>
+      <Login/>
+      </>
   )
 }
 
